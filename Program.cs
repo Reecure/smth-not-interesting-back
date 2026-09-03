00c9ce0633b8a58eeb36a9ab0155f1869f33b3e4
p.yurii <ogogan45@gmail.com>
2026-09-03 18:59:42 +0300

    fix logic

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using WebApplication1;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,10 +10,25 @@ builder.Services.AddCors(o => o.AddDefaultPolicy(p => p
     .AllowAnyMethod()
     .AllowCredentials()));
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
 var app = builder.Build();
 
+app.UseForwardedHeaders();
 app.UseCors();
-app.MapHub<ShakeHub>("/hub/shake");
+
+app.MapGet("/health", () => Results.Ok("OK"));
+
+app.MapHub<ShakeHub>("/hub/shake", options =>
+{
+    options.Transports = Microsoft.AspNetCore.Http.Connections.HttpTransportType.WebSockets |
+                         Microsoft.AspNetCore.Http.Connections.HttpTransportType.LongPolling;
+});
 
 app.MapGet("/api/room", () => Results.Ok(new
 {
